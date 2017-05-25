813f14ba054bc3a7e79f7e2b532bfe907de109aa
perry <235711131719k@gmail.com>
2017-05-25 16:35:55 +0900

    first test

---
+++ line_api/linebot/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+
+def post_text(reply_token, text):
+    header = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {ENTER_ACCESS_TOKEN}"
+    }
+    payload = {
+          "replyToken":reply_token,
+          "messages":[
+                {
+                    "type":"text",
+                    "text": text
+                }
+            ]
+    }
+    requests.post(REPLY_ENDPOINT, headers=header, data=json.dumps(payload))
